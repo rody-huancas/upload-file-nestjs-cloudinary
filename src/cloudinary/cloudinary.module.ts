a139f57f777c556913a523ddaeb3a2a7b36ae891
Rody Huancas <rodyhuancas.04@gmail.com>
2024-09-24 11:46:50 -0500

    Feat: cloudinary provider

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,7 +1,6 @@
-// cloudinary.module.ts
 import { Module } from '@nestjs/common';
-import { CloudinaryProvider } from './cloudinary/cloudinary';
 import { CloudinaryService } from './cloudinary.service';
+import { CloudinaryProvider } from './cloudinary.provider';
 
 @Module({
   providers: [CloudinaryProvider, CloudinaryService],
